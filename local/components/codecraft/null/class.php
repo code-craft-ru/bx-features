845b4ed6345fbd73d110466c7f2dd238bd051924
Roman Shershnev <manriel@mail.ru>
2015-12-27 18:55:47 +0300

    Null component: class.php full support

--- local/components/codecraft/null/class.php
+++ local/components/codecraft/null/class.php
@@ -2,21 +2,84 @@
     die();
 }
 
-class CCodeCraftNullComponent extends CBitrixComponent
-{
-    /**
-     * @param string $testString
-     *
-     * @return string
-     */
-    public function getTestString ($testString = 'test') {
-        return $testString;
+use \Bitrix\Main\Loader,
+    \Bitrix\Main\Localization\Loc,
+    \Bitrix\Main\SystemException;
+
+class CCodeCraftNullComponent extends \CBitrixComponent {
+
+    protected $requiredModules = [];
+
+    protected function isAjax() {
+        return isset($_REQUEST['ajax']) && $_REQUEST['ajax'] == 'y';
+    }
+
+    protected function checkModules() {
+        foreach ($this->requiredModules as $moduleName) {
+            if (!Loader::includeModule($moduleName)) {
+                throw new SystemException(Loc::getMessage('COMPONENT_NULL_NO_MODULE', ['#MODULE#', $moduleName]));
+            }
+        }
+
+        return $this;
     }
 
     /**
-     * @return bool
+     * Event called from includeComponent before component execution.
+     * Takes component parameters as argument and should return it formatted as needed.
+     *
+     * @param  array[string]mixed $arParams
+     * @return array[string]mixed
      */
-    public function getFalse () {
-        return false;
+    public static function onPrepareComponentParams($params) {
+        return $params;
+    }
+
+    /**
+     * Event called from includeComponent before component execution.
+     * Includes component.php from within lang directory of the component.
+     *
+     * @return void
+     */
+    public function onIncludeComponentLang() {
+        $this->includeComponentLang(basename(__FILE__));
+        Loc::loadMessages(__FILE__);
+    }
+
+    protected function prepareResult() {
+        $this->arResult['TEST_STRING'] = 'Test string';
+
+        return $this;
+    }
+
+    public function executeComponent() {
+        global $APPLICATION;
+
+        try {
+            $this->checkModules()->prepareResult();
+
+            if ($this->isAjax()) {
+                $APPLICATION->restartBuffer();
+                echo json_encode([
+                                     'status' => 'ok',
+                                     'data'   => $this->arResult['TEST_STRING']
+                                 ], JSON_FORCE_OBJECT);
+                die();
+            }
+
+            $this->includeComponentTemplate();
+        } catch (SystemException $e) {
+
+            if ($this->isAjax()) {
+                $APPLICATION->restartBuffer();
+                echo json_encode([
+                                     'status' => 'error',
+                                     'data'   => $e->getMessage()
+                                 ], JSON_FORCE_OBJECT);
+                die();
+            }
+
+            self::__showError($e->getMessage());
+        }
     }
 }
--- local/components/codecraft/null/lang/ru/component.php
+++ local/components/codecraft/null/lang/ru/component.php
@@ -2,4 +2,5 @@
     die();
 }
 
+$MESS['COMPONENT_NULL_NO_MODULE'] = 'Модуль "#MODULE#" не установлен.';
 $MESS['COMPONENT_NULL_ERROR_MESSAGE'] = 'Ошибка';
