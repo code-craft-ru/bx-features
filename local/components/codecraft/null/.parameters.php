612ce1f8ae450f26ba94720e1ec2b6a07b20ec8c
pandmitr <thor_work@yahoo.com>
2016-01-04 03:59:10 +0300

    Fix language messages loading; added example parameters
    By pandmitr

--- local/components/codecraft/null/.parameters.php
+++ local/components/codecraft/null/.parameters.php
@@ -4,4 +4,17 @@
 
 use Bitrix\Main\Localization\Loc;
 
-$arComponentParameters = [];
+Loc::loadMessages(__FILE__);
+
+$arComponentParameters = ['PARAMETERS' => ['STRING_PARAMETER'   => ['PARENT'  => 'BASE',
+                                                                    'NAME'    => Loc::getMessage('COMPONENT_NULL_STRING_PARAMETER'),
+                                                                    'TYPE'    => 'STRING',
+                                                                    'DEFAULT' => '',],
+                                           'CHECKBOX_PARAMETER' => ['PARENT'  => 'BASE',
+                                                                    'NAME'    => Loc::getMessage('COMPONENT_NULL_CHECKBOX_PARAMETER'),
+                                                                    'TYPE'    => 'CHECKBOX',
+                                                                    'DEFAULT' => 'Y',],
+                                           'CACHE_TIME'         => ['DEFAULT' => 36000000],
+                                           'AJAX_MODE'          => []
+
+]];
